681e36a0a4cd98d72793feae6aef4e3b5f1c574a
mikushpigel <shpigelmika@gmail.com>
2022-06-07 12:15:12 +0300

    add constactor function

---
+++ start.js
@@ -0,0 +1,30 @@
+console.log('hello world');
+ const user = {
+            firstName: 'moshe',
+            lastName: 'Pretz',
+            age: 45,
+            fullName: function () {
+                return `${this.firstName} ${this.lastName}`;
+            }
+        }
+         const user2 = {
+            firstName: 'rotem',
+            lastName: 'shpigel',
+            age: 25,
+            fullName: function () {
+                return `${this.firstName} ${this.lastName}`;
+            }
+        }
+
+        //קונסקרטור פנקשן- שם הפונקציה תמיד יתחיל באות גדולה
+        function Person(fname, lname, age) {
+            this.firstName = fname;
+            this.lastName = lname;
+            this.age = age;
+        }
+        Person.prototype.greet = function () {
+            return `Hello ${this.firstName} ${this.lastName} ${this.age} welcome to our site!`;
+        }
+
+        let avi = new Person('avi', 'Peretz', 52);
+        let sima = new Person('sima', 'hadad', 60);
